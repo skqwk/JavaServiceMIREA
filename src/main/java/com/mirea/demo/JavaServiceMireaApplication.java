43d2d63792d8d499e683ecf3346b5b75c248749b
skqwk <71013663+skqwk@users.noreply.github.com>
2021-12-11 12:20:10 +0300

    init commit

---
+++ src/main/java/com/mirea/demo/JavaServiceMireaApplication.java
@@ -0,0 +1,13 @@
+package com.mirea.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaServiceMireaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaServiceMireaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mirea/demo/JavaServiceMireaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mirea.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaServiceMireaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
