afbdad33f27b37874a00ad50f641e7c29efbadc9
skqwk <71013663+skqwk@users.noreply.github.com>
2021-12-17 21:44:01 +0300

    add DELETE & PUT to RequestController

--- src/main/java/com/mirea/demo/RequestController.java
+++ src/main/java/com/mirea/demo/RequestController.java
@@ -13,16 +13,28 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class RequestController {
     @PostMapping()
-    @ApiOperation(value = "Получение post-запроса",
-            notes = "Получение post-запроса")
+    @ApiOperation(value = "Получение post-запроса", notes = "Получение post-запроса")
     public ResponseEntity<String> createRequest(@RequestBody String requestString) {
         return ResponseEntity.ok(String.format("Hello %s", requestString));
     }
 
     @GetMapping("{name}")
-    @ApiOperation(value = "Получение get-запроса",
-            notes = "Получение данных get-запроса")
+    @ApiOperation(value = "Получение get-запроса", notes = "Получение данных get-запроса")
     public ResponseEntity<String> getRequest(@ApiParam("Идентификатор запроса") @PathVariable String name) {
         return ResponseEntity.ok(String.format("Hello %s", name));
+    }
+
+    @DeleteMapping("{name}")
+    @ApiOperation(value = "Запрос на удаление",
+            notes = "Получение данных запроса на удаление")
+    public ResponseEntity<String> deleteRequest(@ApiParam("Идентификатор для удаления") @PathVariable String name) {
+        return ResponseEntity.ok(String.format("%s was deleted", name));
+    }
+
+    @PutMapping("{name}")
+    @ApiOperation(value = "Запрос на обновление",
+            notes = "Получение данных запроса на обновление")
+    public ResponseEntity<String> putRequest(@ApiParam("Идентификатор для обновления") @PathVariable String name) {
+        return ResponseEntity.ok(String.format("%s was updated", name));
     }
 }
